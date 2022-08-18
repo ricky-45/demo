5ebaca0c9f4e898efcc824fcfd5166473de21bd0
ricky-45 <rickymhatre6236@gmail.com>
2022-08-18 17:40:41 +0530

    Added new method of subtract

--- src/basic/Math.java
+++ src/basic/Math.java
@@ -7,7 +7,7 @@ public class Math {
 		System.out.println("sum is"+a+b);
 	}
 	
-	public void add(int a,int b,int c)
+	public static void add(int a,int b,int c)
 	{
 		System.out.println("sum is"+a+b+c);
 	}
@@ -17,4 +17,10 @@ public class Math {
 		System.out.println("Product is"+a*b);
 	}
 
+	public static void sub(int a ,int b)
+	{
+		System.out.println("sub is"+ (a-b));
+	}
+
 }
+
