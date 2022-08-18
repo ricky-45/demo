c946381cda607f647c31ae8a4ac85a32fffe63e1
email <abhishek.gupta@worldline.com>
2022-08-18 16:33:45 +0530

    Abhishek

---
+++ src/basic/calculator.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class calculator {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Addition");
+		
+		Math.add(22,67);
+		
+		Math.add(22,67,22);
+		
+		System.out.println("Multiplication");
+		
+		Math.mul(22,6);
+		
+	}
+
+}
