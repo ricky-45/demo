c8027d81ab8127195b4c19a21ad29525de5db98d
email <abhishek.gupta@worldline.com>
2022-08-18 15:57:49 +0530

    file added

---
+++ src/basic/Math.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class Math {
+	
+	public static void add(int a,int b)
+	{
+		System.out.println("sum is"+a+b);
+	}
+	
+	public void add(int a,int b,int c)
+	{
+		System.out.println("sum is"+a+b+c);
+	}
+	
+	public static void mul(int a,int b)
+	{
+		System.out.println("Product is"+a*b);
+	}
+
+}
